6ebd0cfb003b76cfbd307014bec4a4c8796c822f
schairez <34615630+schairez@users.noreply.github.com>
2020-05-14 13:13:01 -0700

    dash ohlc working

--- main.py
+++
@@ -1,8 +0,0 @@
-from stocktools.json_to_df import json_to_df
-from dashboard.plot_ohlc import plot_dashboard
-
-
-if __name__ == "__main__":
-
-    aapl_df = json_to_df('data_aapl.json', path="data/data_raw/data_aapl.json")
-    plot_dashboard(aapl_df, title="Apple Stock")
--- stocktools/json_to_df.py
+++ stocktools/json_to_df.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+
+
 import json
 import pandas as pd
 
